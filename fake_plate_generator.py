228e076c8c08641cf94bf55be3d0a3d2f685d846
Bailey Yu <bailey.yu@yahoo.com>
2019-08-12 16:49:18 -0700

    added square font

--- fake_plate_generator.py
+++ fake_plate_generator.py
@@ -22,8 +22,8 @@ noise_range = args.noise
 rank_blur = args.rank_blur
 brightness = args.brightness
 motion_blur = args.motion_blur
-number_dir = [fake_resource_dir + "/numbers/",fake_resource_dir + "/numbers1/" ]
-letter_dir = [fake_resource_dir + "/letters/" ,fake_resource_dir + "/letters1/"]
+number_dir = [fake_resource_dir + "/numbers/",fake_resource_dir + "/numbers1/",fake_resource_dir + "/numbers2/" ]
+letter_dir = [fake_resource_dir + "/letters/" ,fake_resource_dir + "/letters1/",fake_resource_dir + "/letters2/" ]
 plate_dir = [fake_resource_dir + "/plate_background_use/" ,fake_resource_dir + "/plate_background_use1/"]
 screw_dir = [fake_resource_dir + "/screw/",fake_resource_dir + "/screw1/"]
 
@@ -35,7 +35,7 @@ plate_y_size = 150
 
 class FakePlateGenerator():
     def __init__(self, plate_size):
-        font = random.randint(0,1)
+        font = random.randint(0,2)
         color = random.randint(0,1)
         self.dst_size = plate_size
 
